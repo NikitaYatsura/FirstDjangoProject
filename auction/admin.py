3e0b52dd5f1385db0ef91b264e6748bdf7dab975
NikitaYatsura <NikitaYatsura@outlook.com>
2018-07-23 00:55:52 +0300

    first task

---
+++ auction/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models  import lot, section
+
+# Register your models here.
+
+admin.site.register(lot)
+admin.site.register(section)
---
+++ auction/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class lot(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    date_open = models.DateTimeField()
+    date_close = models.DateTimeField()
+    department = models.ForeignKey(
+        'Section',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class section(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
